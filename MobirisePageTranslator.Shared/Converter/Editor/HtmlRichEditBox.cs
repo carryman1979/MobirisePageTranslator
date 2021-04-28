de591f8ddb1c4cc178dc22b256aeaed213d47120
Flat Dog <carryon1979@googlemail.com>
2021-04-28 18:07:37 +0200

    Prepared for different fonts and replacement places.

--- MobirisePageTranslator.Shared/Converter/Editor/HtmlRichEditBox.cs
+++ MobirisePageTranslator.Shared/Converter/Editor/HtmlRichEditBox.cs
@@ -21,25 +21,82 @@ namespace MobirisePageTranslator.Shared.Converter.Editor
 
     internal struct ReplacementDto
     {
-        public TextType MyProperty { get; set; }
+        public TextType Type { get; set; }
         public int Position { get; set; }
+        public int Length { get; set; }
         public string Attributes { get; set; }
+    }
+
+    internal struct HtmlRtfConverterDto
+    {
+        public TextType Type { get; set; }
+        public Regex HtmlSearchRegex { get; set; }
+        public string RtfReplacementString { get; set; }
+        public Regex RtfSearchRegex { get; set; }
     }
 
     public sealed class HtmlRichEditBox : RichEditBox
     {
         private bool _isHtml = false;
         private const RegexOptions _regexOptions = RegexOptions.Multiline;
-        private Dictionary<Regex, string> _htmlToRtfMatching = new Dictionary<Regex, string>
+        private List<HtmlRtfConverterDto> _htmlRtfConverterData = new List<HtmlRtfConverterDto>
         {
-            { new Regex(@"<h1[\W\D\S]*>([\W\D\S]+)<\/h1[\S]*>", _regexOptions), "\\cf1\\sf36\\b\\ul {0} \\ul0\\b0\\sf0\\line" },
-            { new Regex(@"<h2[\W\D\S]*>([\W\D\S]+)<\/h2[\S]*>", _regexOptions), "\\cf1\\sf32\\b\\ul\\i {0} \\ul0\\b0\\sf0\\line" },
-            { new Regex(@"<h3[\W\D\S]*>([\W\D\S]+)<\/h3[\S]*>", _regexOptions), "\\cf1\\sf28\\b {0} \\b0\\sf0\\line" },
-            { new Regex(@"<h4[\W\D\S]*>([\W\D\S]+)<\/h4[\S]*>", _regexOptions), "\\cf1\\sf26\\b\\ul {0} \\ul0\\b0\\sf0\\line" },
-            { new Regex(@"<h5[\W\D\S]*>([\W\D\S]+)<\/h5[\S]*>", _regexOptions), "\\cf1\\sf24\\b\\ul\\i {0} \\i0\\ul0\\b0\\sf0\\line" },
-            { new Regex(@"<h6[\W\D\S]*>([\W\D\S]+)<\/h6[\S]*>", _regexOptions), "\\cf1\\sf24\\ul {0} \\ul0\\sf0\\line" },
-            { new Regex(@"<p[\W\D\S]*>([\W\D\S]+)<\/p[\S]*>", _regexOptions)  , "\\cf1\\sf20 {0} \\sf0\\line" },
-            { new Regex(@"<a[\W\D\S]*>([\W\D\S]+)<\/a[\S]*>", _regexOptions)  , "\\cf2 {0} \\line" }
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H1,
+                HtmlSearchRegex = new Regex(@"<h1[\W\D\S]*>([\W\D\S]+)<\/h1[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf36\\b\\ul {0} \\ul0\\b0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf32\\b\\ul\\i([\W\D\S]+)\\ul0\\b0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H2,
+                HtmlSearchRegex = new Regex(@"<h2[\W\D\S]*>([\W\D\S]+)<\/h2[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf32\\b\\ul\\i {0} \\ul0\\b0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf32\\b\\ul\\i([\W\D\S]+)\\ul0\\b0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H3,
+                HtmlSearchRegex = new Regex(@"<h3[\W\D\S]*>([\W\D\S]+)<\/h3[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf28\\b {0} \\b0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf28\\b([\W\D\S]+)\\b0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H4,
+                HtmlSearchRegex = new Regex(@"<h4[\W\D\S]*>([\W\D\S]+)<\/h4[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf26\\b\\ul {0} \\ul0\\b0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf26\\b\\ul([\W\D\S]+)\\ul0\\b0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H5,
+                HtmlSearchRegex = new Regex(@"<h5[\W\D\S]*>([\W\D\S]+)<\/h5[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf24\\b\\ul\\i {0} \\i0\\ul0\\b0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf24\\b\\ul\\i([\W\D\S]+)\\i0\\ul0\\b0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.H6,
+                HtmlSearchRegex = new Regex(@"<h6[\W\D\S]*>([\W\D\S]+)<\/h6[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf24\\ul {0} \\ul0\\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf24\\ul([\W\D\S]+)\\ul0\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.P,
+                HtmlSearchRegex = new Regex(@"<p[\W\D\S]*>([\W\D\S]+)<\/p[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf1\\sf20 {0} \\sf0\\line",
+                RtfSearchRegex = new Regex(@"\\cf1\\sf20([\W\D\S]+)\\sf0\\line", _regexOptions)
+            },
+            new HtmlRtfConverterDto
+            {
+                Type = TextType.A,
+                HtmlSearchRegex = new Regex(@"<a[\W\D\S]*>([\W\D\S]+)<\/a[\S]*>", _regexOptions),
+                RtfReplacementString = "\\cf2 {0} \\line",
+                RtfSearchRegex = new Regex(@"\\cf2([\W\D\S]+)\\line", _regexOptions)
+            },
         };
 
         public string HtmlContent
@@ -89,12 +146,11 @@ namespace MobirisePageTranslator.Shared.Converter.Editor
                     "{\\rtf1\\ansi\\deff0{\\fonttbl{\\f0\\fnil\\fcharset0 Arial;}}" +
                     "{\\colortbl;\\red0\\green0\\blue0;\\red0\\green0\\blue210;}";
 
-                _htmlToRtfMatching
-                    .ToList()
+                _htmlRtfConverterData
                     .ForEach(regRepl =>
                     {
                         regRepl
-                            .Key
+                            .HtmlSearchRegex
                             .Matches(htmlText)
                             .Where(x => x.Success)
                             .OrderByDescending(y => y.Index + y.Length)
@@ -105,14 +161,18 @@ namespace MobirisePageTranslator.Shared.Converter.Editor
                                 var formatedValue = match.Groups[1].Value.Replace("\n", " ").Replace("\r", string.Empty).Replace("\t", " ");
                                 var options = RegexOptions.None;
                                 var whitespacesRegex = new Regex("[ ]{2,}", options);
-                                var startIndex = match.Index;
-                                var textLength = match.Length;
-
+                                var replaceDto = new ReplacementDto
+                                {
+                                    Position = match.Index,
+                                    Length = match.Length,
+                                    Type = regRepl.Type,
+                                    Attributes = match.Groups[0].Value
+                                };
 
                                 formatedValue = whitespacesRegex.Replace(formatedValue, " ");
 
                                 // Add to rtf formatted string...
-                                rtfText += string.Format(regRepl.Value, formatedValue);
+                                rtfText += string.Format(regRepl.RtfReplacementString, formatedValue);
                             });
                     });
 
